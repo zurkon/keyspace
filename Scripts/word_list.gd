34b24ebb0e880105c3ac8bd006a89648016bbb51
Zurkon <proflucas.yoshioka@gmail.com>
2021-12-02 14:32:39 -0300

    fixed word_list array duplication

--- Scripts/word_list.gd
+++ Scripts/word_list.gd
@@ -61,22 +61,37 @@ var _rng = RandomNumberGenerator.new()
 
 func _init() -> void:
 	_rng.randomize()
+	print("palavras para jogar: ", _available.size())
 
 
 func get_random_word() -> String:
 	var new_word: String = ""
 	var new_word_index: int = _rng.randi_range(0, _available.size() - 1)
 	
+	# If it's empty, refill
+	if _available.size() == 0:
+		_available = _words
+	
 	for i in _available.size():
+		var first_slice := []
+		var last_slice := []
 		if i == new_word_index:
-			var first_slice = _available.slice(0, i-1)
+			if i != 0:
+				first_slice = _available.slice(0, i-1)
+
 			new_word = _available[i]
 			_filtered.append(new_word)
-			var last_slice = _available.slice(i+1, _available.size() - 1)
+			
+			if i != _available.size() - 1:
+				last_slice = _available.slice(i+1, _available.size() - 1)
+			
 			_available = first_slice
-			_available.append_array(last_slice)
+			
+			if i != _available.size() - 1:
+				_available.append_array(last_slice)
 			break
 	
-	print(_available.size())
+	print("disponivel: ", _available.size())
+	print("filtrado: ", _filtered.size())
 	
 	return new_word
